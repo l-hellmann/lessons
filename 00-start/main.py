d080b36d0c1fbf9ef6c6d600dd07e4bba89d8cb9
Lukáš Hellmann <hellmann@vshosting.cz>
2024-10-22 12:13:01 +0200

    init & start lesson

---
+++ 00-start/main.py
@@ -0,0 +1,11 @@
+# funknce print slouží k vypisování výstupu
+print("super") # vypíše text 'super' na výstup programu
+
+# definování vlastní funkce s jedním povinným argumentem
+def three_printer(arg):
+    print(arg)
+    print(arg)
+    print(arg)
+
+# zavolání funkce s jedním předaným parametrem
+three_printer("cat")
